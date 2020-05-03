481b9a2fed8a0260acd3e8da56b05d77b86c3d5a
Veljko <vmosorinski@gmail.com>
2020-05-03 23:29:52 +0200

    Initial commit

---
+++ src/test/java/com/mvm/MVM/MvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvm.MVM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
